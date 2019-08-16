be8d56405bb4c7f4487b826df597421443b8c816
cuncinc <cuncinc@foxmail.com>
2019-08-16 15:05:28 +0800

    test

--- app/src/main/java/com/cc/creader/MainActivity.java
+++ app/src/main/java/com/cc/creader/MainActivity.java
@@ -14,6 +14,6 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        getSupportActionBar().hide();
+        //getSupportActionBar().hide();
     }
 }
